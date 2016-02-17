5cc7baeb3067584ad3fa9ef6fdfbd27afdec983e
ETS <ETS@dpto01b4>
2016-02-17 10:23:05 +0000

    Este es mi primer cambio

---
+++ ETSGit/src/prueba/Suma.java
@@ -0,0 +1,29 @@
+package prueba;
+public class Suma {
+
+	private int a;
+	private int b;
+
+	public Suma(int x, int y) {
+		this.a = x;
+		this.b = y;
+	}
+
+	public int boli() {
+		return a + b;
+	}
+	
+	public int resta() {
+		return a - b;
+	}
+	
+	public int multi() {
+		return a * b;
+	}
+	
+	public int division() {
+		return a / b;
+	}
+
+	
+}
